7569603251ef2fac97f63538c3358544a577b29e
Bogdan Radu <bogdanrn@gmail.com>
2026-04-27 17:36:45 +0200

    feat(types): PlayerState constants and PlayerStateCode

---
+++ src/playerState.ts
@@ -0,0 +1,10 @@
+export const PlayerState = {
+  UNSTARTED: -1,
+  ENDED: 0,
+  PLAYING: 1,
+  PAUSED: 2,
+  BUFFERING: 3,
+  CUED: 5,
+} as const;
+
+export type PlayerStateCode = (typeof PlayerState)[keyof typeof PlayerState];
