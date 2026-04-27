7f77d8f75fb9e383cb7b6a8ab5f6ec3fd2a7ef3c
Bogdan Radu <bogdanrn@gmail.com>
2026-04-27 18:38:06 +0200

    feat(types): typed addEventListener overloads + types.test-d.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-export { YTEmbed } from './YTEmbed.js';
-export { PlayerState, type PlayerStateCode } from './playerState.js';
 export {
   IframeApiLoadError,
   PlayerDestroyedError,
   PlayerInitError,
 } from './errors.js';
+export type { EventCallbackName } from './eventCallbackNames.generated.js';
+export {
+  type VolumeChangeExtensionOptions,
+  volumeChangeExtension,
+} from './extensions/volumeChange.js';
+export type { FunctionName } from './functionNames.generated.js';
+export { config as ytEmbedConfig, type YTEmbedConfig } from './loadIframeApi.js';
+export { PlayerState, type PlayerStateCode } from './playerState.js';
 export type {
   Extension,
   MethodCallOptions,
@@ -12,10 +18,4 @@ export type {
   YTEmbedEventMap,
   YTEmbedOptions,
 } from './types.js';
-export {
-  type VolumeChangeExtensionOptions,
-  volumeChangeExtension,
-} from './extensions/volumeChange.js';
-export type { FunctionName } from './functionNames.generated.js';
-export type { EventCallbackName } from './eventCallbackNames.generated.js';
-export { config as ytEmbedConfig, type YTEmbedConfig } from './loadIframeApi.js';
+export { YTEmbed } from './YTEmbed.js';
